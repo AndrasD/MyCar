429b8d6c7933283a7f649a6ecb940087674754ec
Dallos András <andras@Dallos-MacBook-Air.local>
2017-02-03 17:04:11 +0100

    nulladik verzió

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'sixishu_andras');
+define('DB_PASSWORD', 'Yaq12wsx01');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'sixishu_gps');
+
+?>
