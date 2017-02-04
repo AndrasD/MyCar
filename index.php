52370b4fc71b74a89687942822d4ae20cba954b2
HOME-WIN10\Andras <andras.dallos@gmail.com>
2017-02-04 12:25:21 +0100

    Responsiv nav-bar és Login-form

--- index.php
+++ index.php
@@ -3,53 +3,61 @@
 
   <head>
     <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-          <title>MyCar App</title>
-          <!-- Bootstrap -->
-          <link href="css/bootstrap.min.css" rel="stylesheet">
-            <link href="css/custom.css" rel="stylesheet">
-              <link href="css/toaster.css" rel="stylesheet">
-                <style>
-                  a {
-                  color: orange;
-                  }
-                </style>
-                <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-                <!--[if lt IE 9]><link href= "css/bootstrap-theme.css"rel= "stylesheet" >
-
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-              </head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>MyCar App</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/toaster.css" rel="stylesheet">
+    <style>
+           a {
+              color: blue;
+             }
+    </style>
+  </head>
 
   <body ng-cloak="">
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="row">
-          <div class="navbar-header col-md-8">
-            <button type="button" class="navbar-toggle" toggle="collapse" target=".navbar-ex1-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" rel="home" title="Where is my car App">MyCar</a>
-          </div>
-          <div class="navbar-header col-md-2">
-            <a class="navbar-brand" rel="home" title="AngularJS Authentication Tutorial" href="http://www.angularcode.com/user-authentication-using-angularjs-php-mysql">Tutorial</a>
-          </div>
+    <nav class="navbar navbar-inverse navbar-fixed-top"  role="navigation">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">MyCar</a>
         </div>
-      </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Username <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a ng-click="logout();"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container" style="margin-top:50px;">
+      <div data-ng-view="" id="ng-view" class="slide-animation"></div>
     </div>
-    <div >
-      <div class="container" style="margin-top:20px;">
+  </body>
+  
+  <hr style="margin-top:10px;margin-bottom:10px;" >
 
-        <div data-ng-view="" id="ng-view" class="slide-animation"></div>
-
-      </div>
-    </body>
   <toaster-container toaster-options="{'time-out': 3000}"></toaster-container>
   <!-- Libs -->
   <script src="js/angular.min.js"></script>
