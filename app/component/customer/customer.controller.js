70f635814379500ad646be8419255a50233c346b
HOME-WIN10\Andras <andras.dallos@gmail.com>
2017-03-16 19:03:36 +0100

    customers view

--- app/component/customer/customer.controller.js
+++ app/component/customer/customer.controller.js
@@ -3,6 +3,15 @@ app.controller('customerController', function ($scope, $rootScope, $routeParams,
     $scope.signup = {};
     $scope.customer = {};
 
+
+    $scope.customersCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+
+
     $scope.signup = {email:'',password:'',name:'',phone:'',address:''};
     $scope.signUp = function (customer) {
         Data.post('signUp', {
