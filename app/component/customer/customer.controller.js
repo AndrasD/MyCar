2cf2f2d9440cb7272059c4a6dc3b4d46b0a9d562
AndrasD <andras.dallos@gmail.com>
2017-03-23 14:49:03 +0100

    customers view with modal add/edit/delet

--- app/component/customer/customer.controller.js
+++ app/component/customer/customer.controller.js
@@ -1,9 +1,9 @@
 app.controller('customerController', function ($scope, $rootScope, $routeParams, $location, $http, Data) {
     //initially set those objects to null to avoid undefined error
-    $scope.customersCollection = {};
+    $scope.customersCollection = [{name: '', email: '', created: '', admin: ''}];
 
     $scope.sortType     = 'name';     // set the default sort type
-    $scope.sortReverse  = false;      // set the default sort order
+    $scope.sortReverse  = false;      // set the default sort reverse
     $scope.searchCustomers  = '';     // set the default search/filter term
 
     Data.get('customers').then(function (results) {
